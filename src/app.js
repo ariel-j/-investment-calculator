fb139588ce52f2609844e9ebb3b40c162c9a1e3d
arieljay <156934721+ariel-j@users.noreply.github.com>
2025-02-26 11:45:36 +0000

    fix deploy

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import InvestmentCalculator from './index';
-
+import InvestmentCalculator from './InvestmentCalculator'
 function App() {
   return (
     <div className="p-4">
